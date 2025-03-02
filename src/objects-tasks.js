7d323030ee01d8131530b789a1777f39525e7556
Elena Khadasevich <camatsu90@mail.ru>
2025-03-02 22:44:33 +0300

    feat: add task 9

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -191,8 +191,15 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const r = {
+    width: `${width}`,
+    height: `${height}`,
+    getArea: () => {
+      return r.width * r.height;
+    },
+  };
+  return r;
 }
 
 /**
