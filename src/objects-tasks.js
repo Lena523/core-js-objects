bc096be863d3bb7f1e08e1d22fbd45f17cf5941a
Elena Khadasevich <camatsu90@mail.ru>
2025-03-02 23:52:25 +0300

    feat: add task 12

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -259,8 +259,24 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  const sortedArr = arr;
+  sortedArr.sort((a, b) => {
+    if (a.country < b.country) {
+      return -1;
+    }
+    if (a.country > b.country) {
+      return 1;
+    }
+    if (a.city < b.city) {
+      return -1;
+    }
+    if (a.city > b.city) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedArr;
 }
 
 /**
